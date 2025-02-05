a7201bdc8776e8fbe636e4bc19d08376acb6c678
Philip Hoffman <git@cubt85iz>
2025-02-04 19:34:53 -0500

    Updates script to handle publishing updates for IPv6 records.

--- update-ddns-address.sh
+++ update-ddns-address.sh
@@ -1,37 +1,73 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
 
-set -euo pipefail
+set -euox pipefail
 
-# Retrieve IPV4 address for WAN.
+update_dns_record(){
+  # Parse DNS record type
+  TYPE="$1"
+  if [ -z "$TYPE" ]; then
+    echo "DNS record type not provided."
+    exit 1
+  fi
+
+  # Parse IP address
+  IP="$2"
+  if [ -z "$IP" ]; then
+    echo "IP address not provided."
+    exit 1
+  fi
+
+  # Get DNS record identifier
+  DNS_RECORD_ID=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?name=${DOMAIN}&type=${TYPE}" \
+    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" -H "X-Auth-Key: $CLOUDFLARE_API_KEY" | jq -r '.result[0].id')
+
+  if [ -n "$DNS_RECORD_ID" ]; then
+
+    # Ref: https://developers.cloudflare.com/api/resources/dns/subresources/records/methods/update/
+    curl https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID \
+        -X PUT \
+        -H 'Content-Type: application/json' \
+        -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+        -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+        -d "{
+          \"type\": \"${TYPE}\",
+          \"name\": \"${DOMAIN}\",
+          \"content\": \"${IP}\",
+          \"ttl\": 3600,
+          \"proxied\": ${PROXIED}
+        }"
+  else
+    echo "ERROR: Unable to locate DNS record identifier."
+    exit 1
+  fi
+}
+
+# Retrieve IPv4 address for WAN.
 WAN_IPV4=$(/usr/bin/dig @resolver4.opendns.com myip.opendns.com +short -4)
 if [ -z "$WAN_IPV4" ]; then
   echo "Unable to retrieve IPv4 address for WAN."
   exit 1
 fi
 
-DOMAIN_IPV4=$(/usr/bin/dig +short "${DOMAIN}")
+# Retrieve IPv6 address for WAN.
+WAN_IPV6=$(/usr/bin/dig @resolver1.ipv6-sandbox.opendns.com AAAA myip.opendns.com +short -6)
+if [ -z "$WAN_IPV6" ]; then
+  echo "Unable to retrieve IPv6 address for WAN."
+fi
+
+# Get IP addresses for specified domain.
+DOMAIN_IPV4=$(/usr/bin/dig "$DOMAIN" A +short)
+DOMAIN_IPV6=$(/usr/bin/dig "$DOMAIN" AAAA +short)
+
+# Update A-record if there's a mismatch between WAN & DOMAIN IPv4 addresses.
 if [ "$WAN_IPV4" != "$DOMAIN_IPV4" ]; then
-  # Ref: https://developers.cloudflare.com/api/resources/dns/subresources/records/methods/update/
-  curl https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID \
-      -X PUT \
-      -H 'Content-Type: application/json' \
-      -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-      -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
-      -d "{
-        \"comment\": \"Domain verification record\",
-        \"content\": \"${WAN_IPV4}\",
-        \"name\": \"${DOMAIN}\",
-        \"proxied\": ${PROXIED},
-        \"settings\": {
-          \"ipv4_only\": true,
-          \"ipv6_only\": true
-        },
-        \"tags\": [
-          \"owner:dns-team\"
-        ],
-        \"ttl\": 3600,
-        \"type\": \"A\"
-      }"
+  update_dns_record "A" "$WAN_IPV4"
 else
   echo "Domain IPv4 address is accurate. Skipping update."
+fi
+
+if [ "$WAN_IPV6" != "$DOMAIN_IPV6" ]; then
+  update_dns_record "AAAA" "$WAN_IPV6"
+else
+  echo "Domain IPv6 address is accurate. Skipping update."
 fi
